b6c09fe2f801ace69ad4e8c8e10dbd57e0b46e87
isa44zy <162628718+isa44zy@users.noreply.github.com>
2024-05-14 15:25:02 -0300

    face

---
+++ src/css/botaoStyle.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    button: {
+     paddingVertical: 20,
+     borderRadius: 5,
+     marginVertical: 10,
+     borderWidth: 1,
+    },
+    buttonPressed: {
+        backgroundColor: '#0056b3'
+    },
+    buttonText: {
+        color: '#fff',
+        fontSize: 16,
+        textAlign: 'center',
+    }
+  });
+  
---
+++ src/css/style.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    cont: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      marginVertical: 10,
+    },
+    line: {
+      width: '20'
+    }
+  });
+  
